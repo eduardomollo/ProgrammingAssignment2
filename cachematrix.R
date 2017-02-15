b008aef20aaefd13f2601043daaedde2a96f42d8
eduardomollo <eduardomollo@gmail.com>
2017-02-14 22:49:52 -0200

    Revert "solution"
    This reverts commit 8b0db415671819dcdcc3b09bdb6f136ce68102f7.

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,8 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
 makeCacheMatrix <- function(x = matrix()) {
   
   m <- NULL
@@ -16,6 +21,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
